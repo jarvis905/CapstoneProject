03ca443145f41c085f1e73d109a4b7fcaa25c8e7
Mohammad.E Eslami <mohamad.slami@gmail.com>
2023-10-27 22:35:48 -0700

    Improve DB Models
    Add some tables and fields to the database

---
+++ CapstoneProject/Data/Migrations/20231028050512_InitialCreate01.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapstoneProject.Migrations
+{
+    public partial class InitialCreate01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Genre",
+                table: "Movies",
+                newName: "Writer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Actores",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "BoxOffice",
+                table: "Movies",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MovieRating",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RunTime",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Actores",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "BoxOffice",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "MovieRating",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "RunTime",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "Writer",
+                table: "Movies",
+                newName: "Genre");
+        }
+    }
+}
